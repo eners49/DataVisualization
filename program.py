c1fad40f16cbc68f2b6c688c7d05496801fcfc64
Stefan Popescu <stefan.popes@gmail.com>
2023-07-18 11:00:26 -0400

    Created new version of program

--- program.py
+++ program.py
@@ -1,13 +1,7 @@
-import numpy as np
 import pandas as pd
 
-def createnew(df: pd.DataFrame, columns: set[any]):
-    cols_total: set[any] = set(df.columns)
-    diff: set[any] = cols_total - columns
-    df.drop(diff, axis = 1, inplace = True)
-
 df = pd.read_csv("data.csv")
-
-createnew(df, {"NAME", "CENSUS2010POP", "ESTIMATESBASE2010", "POPESTIMATE2010", "POPESTIMATE2011", "POPESTIMATE2012", "POPESTIMATE2013", "POPESTIMATE2014", "POPESTIMATE2015", "POPESTIMATE2016", "POPESTIMATE2017", "POPESTIMATE2018", "POPESTIMATE2019"})
+df = df[["NAME", "RBIRTH2015", "RDEATH2015", "RNETMIG2015"]]
+df = df.drop(df.index[[0, 1, 2, 3, 4]])
 print(df)
-df.to_csv("new_data.csv")
+df.to_csv("new_data.csv", index = False)
